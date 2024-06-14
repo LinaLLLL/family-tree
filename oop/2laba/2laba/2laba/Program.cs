f182b20cf37366c0badb09404b55816ce954debf
Lina <linachugainova0@gmail.com>
2024-06-14 10:55:13 +0500

    1 задание почти

---
+++ oop/2laba/2laba/2laba/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите количество чисел в одномерном массиве:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[n];
+        CreateOneDimensionalArray(n, arr);
+        ShowArray(arr);
+
+    }
+
+    static void CreateOneDimensionalArray(int n, int[] arr)
+    {
+        
+        Random rnd = new Random();
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = rnd.Next(0, 21);
+        }
+
+    }
+
+    static void ShowArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+}
