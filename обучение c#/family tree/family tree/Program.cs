5bd2e664b41ed69433e27e0b5d3106ba1ea845c4
Lina <linachugainova0@gmail.com>
2024-08-19 17:52:57 +0500

    Создание класса

---
+++ обучение c#/family tree/family tree/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualBasic.FileIO;
+
+public class FamilyMembers
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    Gender gender { get; set; }
+
+
+
+
+}
+
+public enum Gender {male, female};
