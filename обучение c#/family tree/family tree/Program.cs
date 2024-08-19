3844a0db8e7d1219a85e28f3d860d20eb3f53490
Lina <linachugainova0@gmail.com>
2024-08-19 19:05:52 +0500

    Добавление членов семьи

--- обучение c#/family tree/family tree/Program.cs
+++ обучение c#/family tree/family tree/Program.cs
@@ -1,14 +1,58 @@
-﻿using Microsoft.VisualBasic.FileIO;
+﻿using family_tree;
+using Microsoft.VisualBasic.FileIO;
 
-public class FamilyMembers
+public class Program
 {
-    public string Name { get; set; }
-    public int Age { get; set; }
-    Gender gender { get; set; }
+    public static void Main(string[] args)
+    {
+        FamilyMembers GrandFatherOne = new FamilyMembers()
+        {
+            FullName = "Иванов Иван Иванович",
+            Age = 68,
+            gender = Gender.male,
+        };
+        FamilyMembers GrandFatherTwo = new FamilyMembers()
+        {
+            FullName = "Петров Николай Иванович",
+            Age = 75,
+            gender = Gender.male,
+        };
+        FamilyMembers GrandMatherOne = new FamilyMembers()
+        {
+            FullName = "Иванова Галина Сергеевна",
+            Age = 56,
+            gender = Gender.female,
+        };
+        FamilyMembers GrandMatherTwo = new FamilyMembers()
+        {
+            FullName = "Петрова Лариса Владиславовна",
+            Age = 70,
+            gender = Gender.female,
+        };
+        FamilyMembers Mother = new FamilyMembers()
+        {
+            FullName = "Иванова Мария Николаевна",
+            Age = 43,
+            gender = Gender.female,
+            Mother = GrandMatherTwo,
+            Father = GrandFatherTwo,
+        };
+        FamilyMembers Father = new FamilyMembers()
+        {
+            FullName = "Иванов Геннадий Иванович",
+            Age = 49,
+            gender = Gender.male,
+            Mother = GrandMatherOne,
+            Father = GrandFatherOne,
+        };
+        FamilyMembers Son = new FamilyMembers()
+        {
+            FullName = "Иванов Роман Геннадьевич",
+            Age = 23,
+            gender = Gender.male,
+            Mother = Mother,
+            Father = Father,
+        };
 
-
-
-
+    }
 }
-
-public enum Gender {male, female};
