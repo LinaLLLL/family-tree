6b375596b8bbd58947b6c18369fcb27e99022dd2
Lina <linachugainova0@gmail.com>
2024-08-19 18:19:48 +0500

    Изменение файлов

--- обучение c#/family tree/family tree/FamalyMembers.cs
+++ обучение c#/family tree/family tree/FamalyMembers.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace family_tree
 {
-    internal class Class1
+    public class FamilyMembers
     {
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        Gender gender { get; set; }
+        FamilyMembers Mother { get; set; }
+        FamilyMembers Father { get; set; }
+
+        public FamilyMembers[] GetGrandMother()
+        {
+            return new FamilyMembers[] { Mother.Mother, Father.Mother };
+        }
+        public FamilyMembers[] GetGrandFather()
+        {
+            return new FamilyMembers[] { Father.Father, Mother.Father };
+        }
+        public FamilyMembers[] GetParents()
+        {
+            return new FamilyMembers[] { Mother, Father };
+        }
+        //public FamilyMembers GetChildren()
+        //{
+        //    //return new FamilyMembers ;
+        //}
+
     }
+    public enum Gender { male, female }
 }
